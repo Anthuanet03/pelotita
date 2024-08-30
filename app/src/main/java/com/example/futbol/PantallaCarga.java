055a8b096f21b639be0379a18156e60574b8de75
Anthuanet03 <1475328@senati.pe>
2024-08-29 21:26:39 -0500

    mi primer commit

---
+++ app/src/main/java/com/example/futbol/PantallaCarga.java
@@ -0,0 +1,29 @@
+package com.example.futbol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class PantallaCarga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_carga);
+
+        int Tiempo= 3000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(PantallaCarga.this, MainActivity.class));
+                finish();
+            }
+        }, Tiempo);{
+
+        }
+    }
+}
